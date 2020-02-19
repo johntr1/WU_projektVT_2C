da7d28d71221d5a11c6e3963cb7a141d25cf81ea
john-tran <55193013+johntr1@users.noreply.github.com>
2020-02-19 14:08:26 +0100

    bra

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.navLinks')
+const hamburger = document.querySelector('.hamburger')
+
+function toggleMeny() {
+    nav.classList.toggle('navActive')
+    hamburger.classList.toggle('is-active')
+}
+
+hamburger.addEventListener('click', toggleMeny)
