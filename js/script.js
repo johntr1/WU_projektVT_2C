3302999dae92adb8e817dfef055fb5a2c6a80b7c
john-tran <55193013+johntr1@users.noreply.github.com>
2020-03-20 20:13:36 +0100

    Modals

--- js/script.js
+++ js/script.js
@@ -8,28 +8,39 @@ function toggleMeny() {
 
 hamburger.addEventListener('click', toggleMeny)
 
-smoothScroll.init();
-
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelectorAll(button.dataset.modalTarget)
-        openModal(modal)
-    })
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
 })
 
 closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
 })
 
 function openModal(modal) {
-    if (modal == null) return
-    modal.classlist.add('active')
-    overlay.classList.add('active')
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
 }
